a0d5c8551bdec51c20a860ad3cfa847d3498a565
arseny-chebyshev <arseny.chebyshev@gmail.com>
2022-06-02 02:27:04 +0500

    deployment version

--- bot.py
+++ bot.py
@@ -1,9 +1,23 @@
 from aiogram import executor
+from aiogram.utils.executor import start_webhook
 import logging
-from loader import dp
+from loader import dp, bot
+from settings import webhook_url, webhook_path, webapp_host, webapp_port
 import handlers.admin
 
-logging.basicConfig(level=logging.DEBUG)
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(webhook_url, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
 
 if __name__ == '__main__':
-    executor.start_polling(dispatcher=dp)
+    logging.basicConfig(level=logging.DEBUG)
+#    executor.start_polling(dispatcher=dp)
+    start_webhook(dispatcher=dp, webhook_path=webhook_path,
+                  skip_updates=True, on_startup=on_startup,
+                  on_shutdown=on_shutdown, host=webapp_host,
+                  port=webapp_port)
--- settings.py
+++ settings.py
@@ -6,3 +6,9 @@ bot_token = os.getenv('BOT_TOKEN')
 admin_id = os.getenv('ADMIN_ID')
 post_channel = os.getenv('POST_CHANNEL_ID')
 dikidi_org = os.getenv('DIKIDI_ORG')
+heroku_app = os.getenv('HEROKU_APP')
+webhook_host = f'https://{heroku_app}.herokuapp.com'
+webhook_path = f'/webhook/{bot_token}'
+webhook_url = f'{webhook_host}{webhook_path}'
+webapp_host = '0.0.0.0'
+webapp_port = os.getenv('PORT', default=8000)
