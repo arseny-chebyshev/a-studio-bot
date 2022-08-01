709029a07c34ab9858215534afd5747190b8b68a
arseny-chebyshev <arseny.chebyshev@gmail.com>
2022-08-01 19:50:42 +0500

    Обновление данных для авторизации

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ from aiogram import executor
 from aiogram.utils.executor import start_webhook
 import logging
 from loader import dp, bot
-from settings import webhook_url, webhook_path, webapp_host, webapp_port
+from settings import webhook_url, webhook_path, webapp_host, webapp_port, admin_id
 import handlers.admin
 
 
 async def on_startup(dispatcher):
     await bot.set_webhook(webhook_url, drop_pending_updates=True)
-
+#    await bot.send_message(admin_id, "Starting..")
 
 async def on_shutdown(dispatcher):
     pass
@@ -17,8 +17,9 @@ async def on_shutdown(dispatcher):
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-#    executor.start_polling(dispatcher=dp)
+#    executor.start_polling(dispatcher=dp, on_startup=on_startup)
     start_webhook(dispatcher=dp, webhook_path=webhook_path,
                   skip_updates=True, on_startup=on_startup,
                   on_shutdown=on_shutdown, host=webapp_host,
                   port=webapp_port)
+#main()
